2158bde395c5fa4e0d3908c098e37e8959cdd528
Simon Tucker <simon@brotherlogic@gmail>
2019-11-05 20:10:01 -0800

    Tracks all jobs

--- versiontrackerutils.go
+++ versiontrackerutils.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"fmt"
-
 	"golang.org/x/net/context"
 )
 
 func (s *Server) track(ctx context.Context) error {
 	jobs, err := s.slave.list(ctx, s.Registry.Identifier)
 	if err != nil {
-		s.Log(fmt.Sprintf("Found %v jobs", len(jobs)))
+		s.jobs = jobs
 	}
 
 	return nil
