3bb5104e86160f96877dbde466fae6a0e0abf927
Simon Tucker <simon@brotherlogic@gmail>
2019-11-05 19:04:35 -0800

    Adds pulling

--- versiontrackerutils.go
+++ versiontrackerutils.go
@@ -1,4 +1,16 @@
 package main
 
-func doNothing() {
+import (
+	"fmt"
+
+	"golang.org/x/net/context"
+)
+
+func (s *Server) track(ctx context.Context) error {
+	jobs, err := s.slave.list(ctx, s.Registry.Identifier)
+	if err != nil {
+		s.Log(fmt.Sprintf("Found %v jobs", len(jobs)))
+	}
+
+	return nil
 }
