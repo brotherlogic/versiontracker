2776a35da816348d2ffa5e766b6612dd29c6bd39
Simon Tucker <simon@brotherlogic@gmail>
2019-11-05 20:30:26 -0800

    Number 3

--- versiontrackerutils.go
+++ versiontrackerutils.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
+
 	"golang.org/x/net/context"
 )
 
 func (s *Server) track(ctx context.Context) error {
 	jobs, err := s.slave.list(ctx, s.Registry.Identifier)
+	s.Log(fmt.Sprintf("%v and %v", jobs, err))
 	if err != nil {
 		s.jobs = jobs
 	}
