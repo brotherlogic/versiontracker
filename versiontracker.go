959bee0bb26f33521d1f5d4690233466944aae4d
Simon Tucker <brotherlogic@gmail.com>
2020-06-12 11:33:16 -0700

    Bumps the build

--- versiontracker.go
+++ versiontracker.go
@@ -188,7 +188,7 @@ func (s *Server) Mote(ctx context.Context, master bool) error {
 // GetState gets the state of the server
 func (s *Server) GetState() []*pbg.State {
 	return []*pbg.State{
-		&pbg.State{Key: "needs_copy", Text: fmt.Sprintf("%v", s.needsCopy)},
+		&pbg.State{Key: "needs_a_copy", Text: fmt.Sprintf("%v", s.needsCopy)},
 	}
 }
 
