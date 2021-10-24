4e15a4dfb6a10bd44b86077c1925986e7ec17dd7
simon <brotherlogic@gmail.com>
2021-10-24 10:14:35 -0700

    Convert to context log

--- versiontrackerapi.go
+++ versiontrackerapi.go
@@ -24,10 +24,10 @@ var (
 
 //NewVersion a new version is available
 func (s *Server) NewVersion(ctx context.Context, req *pb.NewVersionRequest) (*pb.NewVersionResponse, error) {
-	s.Log(fmt.Sprintf("Looking for %v", req.GetVersion().GetJob().GetName()))
+	s.CtxLog(ctx, fmt.Sprintf("Looking for %v", req.GetVersion().GetJob().GetName()))
 	for _, version := range s.tracking {
 		if version.GetJob().GetName() == req.GetVersion().GetJob().GetName() {
-			s.Log(fmt.Sprintf("Found %v vs %v", version, req.GetVersion()))
+			s.CtxLog(ctx, fmt.Sprintf("Found %v vs %v", version, req.GetVersion()))
 			if version.GetVersionDate() < req.GetVersion().GetVersionDate() {
 				s.tracking[req.GetVersion().GetJob().GetName()] = req.GetVersion()
 				tracking.With(prometheus.Labels{"server": req.GetVersion().GetJob().GetName(), "versiondate": fmt.Sprintf("%v", time.Unix(req.GetVersion().GetVersionDate(), 0))}).Set(float64(len(s.tracking)))
